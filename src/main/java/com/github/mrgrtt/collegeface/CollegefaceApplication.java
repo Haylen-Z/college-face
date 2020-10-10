d0cbfcabece308fc65c244678647b3a5476b0f71
Mrgrtt <grttxiansheng@qq.com>
2020-10-10 15:36:29 +0800

    新增：建立工程项目

---
+++ src/main/java/com/github/mrgrtt/collegeface/CollegefaceApplication.java
@@ -0,0 +1,13 @@
+package com.github.mrgrtt.collegeface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegefaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegefaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/mrgrtt/collegeface/CollegefaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mrgrtt.collegeface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegefaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
