00db56e1e1b1376721d6a16526c9a156e473f207
Mrgrtt <grttxiansheng@qq.com>
2020-10-10 16:48:26 +0800

    新增：登录验证Interceptor

--- src/main/java/com/github/mrgrtt/collegeface/CollegefaceApplication.java
+++ src/main/java/com/github/mrgrtt/collegeface/CollegefaceApplication.java
@@ -1,9 +1,11 @@
 package com.github.mrgrtt.collegeface;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.github.mrgrtt.collegeface.mapper")
 public class CollegefaceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/github/mrgrtt/collegeface/component/AuthenticateInterceptor.java
@@ -0,0 +1,28 @@
+package com.github.mrgrtt.collegeface.component;
+
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 登录认证拦截器
+ */
+public class AuthenticateInterceptor extends HandlerInterceptorAdapter {
+    String[] requirePaths = { "/admin/**" };
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        AntPathMatcher antPathMatcher = new AntPathMatcher();
+        for (String path: requirePaths) {
+            if (antPathMatcher.match(path, request.getRequestURI())) {
+                if (request.getSession().getAttribute("UserId") == null) {
+                    response.getWriter().println("未登录");
+                    return  false;
+                }
+            }
+        }
+        return true;
+    }
+}
